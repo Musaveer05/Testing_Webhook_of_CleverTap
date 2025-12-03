d7bb84c5f076603c23bd1a6c16a10d6757221d90
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2025-12-03 21:03:17 +0530

    Change status endpoint to send 200 OK response without body

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/status', (req, res) => {
-  res.json({ status: 'OK' });
+  res.sendStatus(200);  // sends 200 OK with no body
 });
 
 // Start the server
