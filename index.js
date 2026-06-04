834368fa75163179c8b8db71dafac0d132f83020
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2026-06-04 11:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/send-event", async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
 module.exports = app;
